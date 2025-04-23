c3d9dfe9f3397fb83312d16759a50bdffa026a0c
Clement Delphin <clementcla@outlook.com>
2025-04-23 19:06:42 +0530

    updated project sections

--- src/components/RightSection.tsx
+++ src/components/RightSection.tsx
@@ -9,7 +9,6 @@ const skills = [
     "Redux",
     "AWS",
     "TailwindCSS",
-    "GraphQL",
 ]
 
 const RightSection = () => {
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,6 @@
 export const projects = [
     {
-        title:"Streamify Dashboard",
+        title:"Streamify Dashboard 🎶",
         description:"Displays key metrics & charts for a fictional music streaming service, providing insights into user activity & revenue.",
         techstack: ["React.js","TailwindCSS", "Recharts","React Hooks"],
         image:"/Streamify-Screenshot.png",
@@ -8,25 +8,26 @@ export const projects = [
         sourcelink:"https://github.com/clementrozario/Streamify",
     },
     {
-        title:"Contest Tracker",
-        description:"Fetches upcoming contests from Codeforces & Leetcode, allows filtering, bookmarking, and adding YouTube links for past contests.",
-        techstack: ["React.js","TailwindCSS","Mongodb","Node.js"],
-        livelink:"",
-        sourcelink:""
-    },
-    {
-        title:"Realtime Chat App",
+        title:"Realtime Chat App 💬",
         description:" Built from scratch with WebSockets for real-time messaging, featuring a simple and clean UI.",
         techstack:["React.js","Node.js","TailwindCSS","DaisyUI","JWT","Zustand"],
-        livelink:"",
-        sourcelink:"",
+        livelink:"https://real-time-chat-app-2yz1.onrender.com/",
+        sourcelink:"https://github.com/clementrozario/Real_Time_Chat_App",
     },
     {
-        title:"Expense Tracker App",
-        description:"Add savings & expenses with visual charts, built using GraphQL for efficient data management.",
-        techstack:["React.js","Node.js","Express.js","MongoDB","Apollo GraphQL"],
+        title:"Expense Tracker App 💸",
+        description:"Add savings & expenses with s3 storage leader boards payment integration",
+        techstack:["React.js","Node.js","Express.js","MongoDB"],
         livelink:"",
-        sourcelink:"",
+        sourcelink:"https://github.com/clementrozario/Expense-Tracker_NoSql",
+    },
+
+    {
+        title:"Note taking with AI summary 📝",
+        description:"Add savings & expenses with visual charts, built using GraphQL for efficient data management.",
+        techstack:["Next.js","supabase","tailwindcss","Shadcn","postgresql","react Query"],
+        livelink:"",
+        sourcelink:"https://github.com/clementrozario/NoteTaking-with-AI",
     },
 
 ];
